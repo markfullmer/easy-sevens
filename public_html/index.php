1fbc7a1383518a5d1d7bdb5aeb42d094255a4a28
markfullmer <mfullmer@gmail.com>
2023-05-28 13:20:43 -0700

    Add switcher

--- public_html/index.php
+++ public_html/index.php
@@ -1,5 +1,4 @@
 <html lang="en">
-
   <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -12,20 +11,19 @@
   <meta property="og:site_name" content="Scrabble bingo trainer by Mark Fullmer" />
   <meta property="og:type" content="website">
   <link rel="canonical" href="https://scrabble.markfullmer.com/" />
-
     <style>
       body {
-        font-size: 2rem;
+        font-size: 1rem;
         font-family: monospace;
         margin: auto;
         width: 50%;
       }
       input[type="text"]
       {
-          font-size: 3rem;
-          font-weight: bold;
-          font-family: monospace;
-          text-transform: uppercase;
+        font-size: 3rem;
+        font-weight: bold;
+        font-family: monospace;
+        text-transform: uppercase;
       }
       .firework-wrapper {
         position: relative;
@@ -42,10 +40,22 @@
     </style>
 </head>
 <body>
+<a href="/?list=q-no-u">Q without U words</a> |
+<a href="/?list=ersn">'ERSN' words</a> |
+<a href="/?list=erst">'ERST' words</a> |
+<a href="/?list=ers">'ERS' words</a>
+<hr>
+
 <?php
 
-
-$words = file_get_contents('../ersn.csv');
+$list = 'ersn';
+if (isset($_GET['list'])) {
+  if (in_array($_GET['list'], ['q-no-u', 'ersn', 'erst', 'ers'])) {
+    $list = $_GET['list'];
+  }
+}
+$return = $_SERVER['SERVER_NAME'] . '?list=' . $list;
+$words = file_get_contents('../' . $list . '.csv');
 $list = explode(PHP_EOL, $words);
 $clean = [];
 foreach ($list as $item) {
@@ -60,11 +70,13 @@ if (isset($_REQUEST['guess']) && isset($_REQUEST['actual'])) {
     $streak = $_REQUEST['streak'] ?: 0;
     $streak = (int) $streak;
     $streak++;
+    $next = $streak + 1;
     echo 'Current streak: <div class="firework">' . $streak . '</div>';
-    echo "<h2>Another?</h2>";
+    echo "<h2>Try for $next?</h2>";
   }
   else {
-    echo "<h2>Nope! The word was " . base64_decode($_REQUEST['actual']) . '</h2>';
+    $actual = base64_decode($_REQUEST['actual']);
+    echo "<h2>Nope! The word was <a href='https://dictionary.com/browse/" . $actual . "'>" . $actual . "</a></h2>";
     echo "<h2>Try again?</h2>";
   }
 }
@@ -76,11 +88,13 @@ $random = rand(0, $total);
 $word = trim($list[$random]);
 $stringParts = str_split($word);
 sort($stringParts);
-$shuffled = implode($stringParts); // abc
+$shuffled = implode($stringParts);
+$length = strlen($shuffled);
 echo "<h2>" . strtoupper($shuffled) . "</h2>";
-echo '<form action="//' . $_SERVER['SERVER_NAME'] . '" method="POST">'; ?>
+echo '<form action="//' . $return . '" method="POST">';
+?>
 
-<input type="text" pattern="[<?php echo $shuffled; echo strtoupper($shuffled); ?>]{7}" autofocus title="Must be seven letters, matching <?php echo strtoupper($shuffled); ?>" required name="guess"></input>
+<input type="text" pattern="[<?php echo $shuffled; echo strtoupper($shuffled); ?>]{<?php echo $length; ?>}" autofocus title="Must be seven letters, matching <?php echo strtoupper($shuffled); ?>" required name="guess"></input>
 <input type="hidden" name="actual" value="<?php echo base64_encode($word); ?>" />
 <input type="hidden" name="streak" value="<?php echo $streak; ?>" />
 </form>
